b90b3f5980c95a47cec54e085f60c86d6bab7704
luciobo <luciobo@icloud.com>
2022-12-12 17:16:24 +0100

    creata struttura html

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,22 @@
 
 <template>
-    <div class="text-danger ">
-        ciao
+    <div class="container p-0">
+        <div class="bg-black d-flex align-items-center justify-content-between">
+            <img src="/logonetflix.png" alt="">
+            <div class="input-group p-2 w-25">
+                <input type="text" class="form-control" placeholder="" aria-label="Example text with button addon"
+                    aria-describedby="button-addon1">
+                <button class="btn btn-primary" type="button" id="button-addon1">Cerca</button>
+            </div>
+        </div>
     </div>
+
 </template>
+
+<style>
+
+img {
+    width: 200px;
+}
+
+</style>
