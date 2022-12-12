cc81f647ef283bf1c53e239a6aa37c5caa2b3ecd
luciobo <luciobo@icloud.com>
2022-12-12 16:10:55 +0100

    commit quasi inziale

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import TheHeader from "./components/TheHeader.vue"
+import TheMain from "./components/TheMain.vue"
+
+
+
+export default {
+    components: { TheHeader, TheMain },
+
+    data() {
+        return {
+
+        };
+    },
+    methods: {},
+
+    beforeMount() { },
+}
+</script>
+
+
+<template>
+    <div class="">
+        <div>
+            <TheHeader></TheHeader>
+        </div>
+
+        <div>
+            <TheMain></TheMain>
+        </div>
+    </div>
+    
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,6 @@
+
+<template>
+    <div class="text-danger ">
+        ciao
+    </div>
+</template>
