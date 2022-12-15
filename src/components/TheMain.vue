cf09b2e926dacc4758590fa54af0739ad8f1205e
luciobo <luciobo@icloud.com>
2022-12-15 18:16:57 +0100

    aggiunto hover sulle card e piccoli migliramenti

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -4,16 +4,17 @@
 
         <!-- film -->
         <h1 class="pb-3 text-white">FILM</h1>
-        <div class="g-4 d-flex overflow-scroll">
+        <div class="g-4 d-flex overflow-auto">
             <div class="col-3 px-1" v-for="ciao in store.listaFilm">
                 <div class="card h-100">
                     <img :src="store.img.link + store.img.size + ciao.poster_path" class="card-img-top" alt="">
                     <div class="card-img-overlay text-white mia-card">
-                        <h4 class="card-title">Titolo: {{ciao.title}}</h4>
-                        <h6>Titolo Originale: {{ciao.original_title}}</h6>
-                        <div>Lingua: {{ciao.original_language}}</div>
-                        <div>Voto Medio: {{ciao.vote_average}}</div>
-                        <div>Data Uscita: {{ciao.release_date}}</div>
+                        <h4 class="py-1"> class="card-title">Titolo: {{ciao.title}}</h4>
+                        <h6 class="py-1">Titolo Originale: {{ciao.original_title}}</h6>
+                        <div class="py-1">Lingua: {{ciao.original_language}}</div>
+                        <div class="py-1">Voto Medio: {{ciao.vote_average}}</div>
+                        <div class="py-1">Data Uscita: {{ciao.release_date}}</div>
+                        <p class="py-1">Riepilogo: {{ciao.overview}}</p>
                     </div>
                 </div>
             </div>
@@ -26,11 +27,12 @@
                 <div class="card h-100">
                     <img :src="store.img.link + store.img.size + ciaoSerie.poster_path" class="card-img-top" alt="">
                     <div class="card-img-overlay text-white mia-card">
-                        <h4 class="card-title">Titolo: {{ciaoSerie.name}}</h4>
-                        <h6>Titolo Originale: {{ciaoSerie.original_name}}</h6>
-                        <div>Lingua: {{ciaoSerie.original_language}}</div>
-                        <div>Voto Medio: {{ciaoSerie.vote_average}}</div>
-                        <div>Data Uscita: {{ciaoSerie.first_air_date}}</div>
+                        <h4 class="py-1">class="card-title">Titolo: {{ciaoSerie.name}}</h4>
+                        <h6 class="py-1">Titolo Originale: {{ciaoSerie.original_name}}</h6>
+                        <div class="py-1">Lingua: {{ciaoSerie.original_language}}</div>
+                        <div class="py-1">Voto Medio: {{ciaoSerie.vote_average}}</div>
+                        <div class="py-1">Data Uscita: {{ciaoSerie.first_air_date}}</div>
+                        <p class="py-1">Riepilogo: {{ciaoSerie.overview}}</p>
                     </div>
                 </div>
             </div>
@@ -64,6 +66,7 @@ export default {
 .mia-card:hover {
     opacity: 1;
     background-color: #2f3137af;
+    transition: 1s;
 }
 
 </style>
